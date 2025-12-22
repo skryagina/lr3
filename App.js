393a0684f08f99e417175a674637860ac524aafe
skryagina <144270089+skryagina@users.noreply.github.com>
2025-12-22 22:59:49 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,43 @@
+﻿import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { AuthProvider } from './context/AuthContext';
+
+// Layout
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+
+// Pages
+import HomePage from './pages/Home/HomePage';
+import FilmsPage from './pages/Films/FilmsPage';
+import LoginPage from './pages/Login/LoginPage';
+import RegisterPage from './pages/Register/RegisterPage';
+import FilmDetailPage from './pages/FilmDetail/FilmDetailPage';
+import ReviewsPage from './pages/Reviews/ReviewsPage';
+import AllReviewsPage from './pages/AllReviews/AllReviewsPage';
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+          <Header />
+          <Box component="main" sx={{ flexGrow: 1, py: 3 }}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/films" element={<FilmsPage />} />
+              <Route path="/films/:id" element={<FilmDetailPage />} />
+              <Route path="/reviews" element={<ReviewsPage />} />
+              <Route path="/all-reviews" element={<AllReviewsPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
+            </Routes>
+          </Box>
+          <Footer />
+        </Box>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
---
+++ App.tsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Afanasyeva Films</h1>
+        <p>React приложение успешно настроено!</p>
+        <p>Можно начинать разработку фронтенда.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
